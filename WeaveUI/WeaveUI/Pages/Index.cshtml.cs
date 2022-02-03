fd74b6f1e9c59eb83b9a8af25d8ad5f5b84c5ab9
Johannes <johannes.bertens@gmail.com>
2022-02-03 08:41:36 +0100

    Added sane defaults

--- WeaveUI/WeaveUI/Pages/Index.cshtml.cs
+++ WeaveUI/WeaveUI/Pages/Index.cshtml.cs
@@ -32,6 +32,8 @@ namespace WeaveUI.Pages
         private static readonly HttpClient _httpClient = new HttpClient();
 
         public WeavesObject? weavesObject = new WeavesObject();
+        public string intelligence = "18";
+        public string rank = "0";
         public string commands;
         public string error;
 
@@ -45,6 +47,10 @@ namespace WeaveUI.Pages
 
             if (!string.IsNullOrEmpty(Request.QueryString.Value))
             {
+                // set int & rank
+                intelligence = Request.Query["int"];
+                rank = Request.Query["rank"];
+
                 var result = _httpClient.GetAsync(new Uri("https://weavepracs.azurewebsites.net/api/GetPracs" + Request.QueryString.Value)).Result;
                 string jsonString = result.Content.ReadAsStringAsync().Result;
                 if (result.IsSuccessStatusCode)
@@ -78,7 +84,7 @@ namespace WeaveUI.Pages
             List<string> elementList = new();
             foreach (var kvPair in requestElements)
             {
-                if (int.TryParse(kvPair.Value, out int percentage) && percentage > 0)
+                if (kvPair.Key == "rank" || int.TryParse(kvPair.Value, out int percentage) && percentage > 0)
                     elementList.Add(kvPair.Key + "=" + kvPair.Value);
             }
             var queryString = "?" + string.Join("&", elementList);
